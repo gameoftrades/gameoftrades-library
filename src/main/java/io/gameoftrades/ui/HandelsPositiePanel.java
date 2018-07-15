2916da81d0d5ad426c3c8e1a61f9114c8d128145
Erik Hooijmeijer <erik.hooijmeijer@42.nl>
2018-07-15 18:39:59 +0200

    v0.4.0

--- src/main/java/io/gameoftrades/ui/HandelsPositiePanel.java
+++ src/main/java/io/gameoftrades/ui/HandelsPositiePanel.java
@@ -7,7 +7,6 @@ import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JSeparator;
 
 import io.gameoftrades.model.markt.actie.HandelsPositie;
 
